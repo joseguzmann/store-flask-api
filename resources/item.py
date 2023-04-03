fb0f252767272bd56797b4d7b74abec978dc171b
Jose Guzman <jg175415@gmail.com>
2023-04-02 19:16:44 -0500

    database config

--- resources/item.py
+++ resources/item.py
@@ -3,13 +3,15 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from db import items, stores
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    # Decorate main success response, pasa lo que retornemos por ItemSchema,
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -23,11 +25,9 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data
@@ -38,20 +38,13 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
-    def post(self):
-        item_data = request.get_json()
-
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(400,
-                  message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload."
-                  )
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):  # item_data contains JSON which is the validated fields that Scheme requested
 
         for item in items.values():
             if (
---
+++ schemas.py
@@ -0,0 +1,27 @@
+from marshmallow import Schema, fields
+
+
+class PlainItemSchema(Schema):
+    # dump_only: el dato solo se puede definir durante la serializacion para devolver al front
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)  # Required in both ends
+    price = fields.Float(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
