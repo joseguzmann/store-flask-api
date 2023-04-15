c7d1064da39a2aa9be1adbeadb9e92d16e431aa9
Jose Guzman <jg175415@gmail.com>
2023-04-15 16:55:30 -0500

    authentication

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -33,6 +35,62 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "280605676998343473689219698815199019541"
     jwt = JWTManager(app)
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked", "error": "token_revoked"}
+            ),
+            401
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required"
+                }
+            ),
+            401
+        )
+
+    @jwt.additional_claims_loader  # To add a new claim to JWT
+    def add_claims_to_jwt(identity):
+        # Look in the database and see whether the user is an admin
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired",
+                    "error": "token_expired"}),
+            401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Signature verification failed",
+                    "error": "invalid_token"}),
+            401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({"description": "Request does not contain an access token",
+                    "error": "authorization_required"}),
+            401
+        )
+
     @app.before_first_request
     def create_tables():
         db.create_all()
@@ -40,5 +98,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required, get_jwt
 # Todos los errores de SQLAlchemy heredan de este error
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -12,16 +12,21 @@ from schemas import ItemSchema, ItemUpdateSchema
 blp = Blueprint("Items", __name__, description="Operations on items")
 
 
-@blp.route("/item/<string:item_id>")
+@blp.route("/item/<int:item_id>")
 class Item(MethodView):
     # Decorate main success response, pasa lo que retornemos por ItemSchema,
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):  # Endpoint to get an item
         # Query available with Flask-SQLAlchemy and not with VAnilla SQLAlchemy
         item = ItemModel.query.get_or_404(item_id)
         return item
 
+    @jwt_required()
     def delete(self, item_id):  # Endpoint to delete an item
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin privilege required.")
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
@@ -46,10 +51,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):  # Endpoint to get all items
         return ItemModel.query.all()
 
+    @jwt_required(fresh=True)
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):  # Endpoint to create an item
